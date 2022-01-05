7ae74c02bbb9bb3b4c6419b9b8b3dd3381b411e4
Code Sandbox <hello@codesandbox.io>
2022-01-05 02:38:27 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ad61b76e6d9c1706fabef318ccbef5cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073c7833fbc6baf551dfcff6fefa80d1",
+    "url": "https://mingderwang.github.io/csb-9o3o2/index.html"
+  },
+  {
+    "revision": "215f502ec2254ef30d69",
+    "url": "https://mingderwang.github.io/csb-9o3o2/static/css/main.566e0db3.chunk.css"
+  },
+  {
+    "revision": "45ec1de5e39cea3a2798",
+    "url": "https://mingderwang.github.io/csb-9o3o2/static/js/2.0677692b.chunk.js"
+  },
+  {
+    "revision": "215f502ec2254ef30d69",
+    "url": "https://mingderwang.github.io/csb-9o3o2/static/js/main.236f7b7a.chunk.js"
+  },
+  {
+    "revision": "b2623d0cc0b5d34f0fb7",
+    "url": "https://mingderwang.github.io/csb-9o3o2/static/js/runtime~main.cd77e819.js"
+  }
+]);
